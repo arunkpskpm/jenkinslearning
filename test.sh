449cf48d057ace50ab607b9d21985268033a49f7
arunkpskpm <arunkpskpm@gmail.com>
2021-04-18 18:40:11 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "This is an example of a script"
+echo "This is running in a build step"
+echo "This is the string passed  : $STRING"
+SUM=$(($FIRSTNUMBER + $SECONDNUMBER))
+echo "Sum of FISTNUMBER + SECONDNUMBER is : $SUM"
