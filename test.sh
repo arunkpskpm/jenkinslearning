af95df48cd0d1fe5ac21636f1dfd423605b86aa5
arunkpskpm <arunkpskpm@gmail.com>
2021-04-18 19:33:38 +0530

    Update test.sh
    update test.sh

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@ echo "This is from github.com"
 echo "This is the string passed  : $STRING"
 SUM=$(($FIRSTNUMBER + $SECONDNUMBER))
 echo "Sum of FISTNUMBER + SECONDNUMBER is : $SUM"
-echo "This was run automatically"
+echo "This was run automatically triggered by webhook"
